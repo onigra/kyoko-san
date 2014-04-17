1b2eaaf3f0da369e035f99a0669d793608fed3a6
onigra <3280467rec@gmail.com>
2014-04-17 21:11:46 +0900

    add auth_token column

--- db/migrate/20140417112743_sorcery_core.rb
+++ db/migrate/20140417112743_sorcery_core.rb
@@ -4,10 +4,12 @@ class SorceryCore < ActiveRecord::Migration
       t.string :email,            :null => false
       t.string :crypted_password, :null => false
       t.string :salt,             :null => false
+      t.string :auth_token,       :null => true
 
       t.timestamps
     end
 
     add_index :users, :email, unique: true
+    add_index :users, :auth_token, unique: true
   end
 end
