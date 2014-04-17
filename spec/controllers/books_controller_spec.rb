d533e8a61303445bde08dbb12a20e9d11dcac8a5
onigra <3280467rec@gmail.com>
2014-04-17 14:33:50 +0900

    リファクタリング

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,19 +1,20 @@
 require 'spec_helper'
+
 describe BooksController do
   let(:valid_attributes) { { "name" => "MyString", "isbn" => "12345" } }
+  let(:book) { Book.create! valid_attributes }
+  before { book }
 
   describe "GET index" do
     it "assigns all books as @books" do
-      book = Book.create! valid_attributes
       get :index
-      assigns(:books).should eq([book])
+      expect(assigns :books).to eq [book]
     end
   end
 
   describe "GET show" do
     it "assigns the requested book as @book" do
-      book = Book.create! valid_attributes
-      get :show, {:id => book.to_param}
+      get :show, { :id => book.to_param }
       assigns(:book).should eq(book)
     end
   end
@@ -22,22 +23,22 @@ describe BooksController do
     describe "with valid params" do
       it "creates a new Book" do
         expect {
-          post :create, {:book => valid_attributes}
+          post :create, { :book => valid_attributes }
         }.to change(Book, :count).by(1)
       end
 
       it "assigns a newly created book as @book" do
-        post :create, {:book => valid_attributes}
-        assigns(:book).should be_a(Book)
-        assigns(:book).should be_persisted
+        post :create, { :book => valid_attributes }
+        expect(assigns :book).to be_a Book
+        expect(assigns :book).to be_persisted
       end
     end
 
     describe "with invalid params" do
       it "assigns a newly created but unsaved book as @book" do
         Book.any_instance.stub(:save).and_return(false)
-        post :create, {:book => { "name" => "invalid value" }}
-        assigns(:book).should be_a_new(Book)
+        post :create, { :book => { "name" => "invalid value" } }
+        expect(assigns :book).to be_a_new Book
       end
     end
   end
@@ -45,31 +46,27 @@ describe BooksController do
   describe "PUT update" do
     describe "with valid params" do
       it "updates the requested book" do
-        book = Book.create! valid_attributes
-        Book.any_instance.should_receive(:update).with({ "name" => "MyString" })
-        put :update, {:id => book.to_param, :book => { "name" => "MyString" }}
+        put :update, { :id => book.to_param, :book => { "name" => "MyString" } }
+        expect(assigns(:book).name).to eq "MyString"
       end
 
       it "assigns the requested book as @book" do
-        book = Book.create! valid_attributes
         put :update, {:id => book.to_param, :book => valid_attributes}
-        assigns(:book).should eq(book)
+        expect(assigns :book).to eq book
       end
     end
 
     describe "with invalid params" do
       it "assigns the book as @book" do
-        book = Book.create! valid_attributes
         Book.any_instance.stub(:save).and_return(false)
         put :update, {:id => book.to_param, :book => { "name" => "invalid value" }}
-        assigns(:book).should eq(book)
+        expect(assigns :book).to eq book
       end
     end
   end
 
   describe "DELETE destroy" do
     it "destroys the requested book" do
-      book = Book.create! valid_attributes
       expect {
         delete :destroy, {:id => book.to_param}
       }.to change(Book, :count).by(-1)
