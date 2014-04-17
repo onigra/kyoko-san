988674fa6c6373f5e16f04de1287dd37ff5b1885
onigra <3280467rec@gmail.com>
2014-04-17 22:38:25 +0900

    とりあえず完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+  before_action :auth_token_check
+
+  def current_user
+    return nil unless params[:auth_token]
+    User.where(auth_token: params[:auth_token]).first
+  end
+
+  private
+    def auth_token_check
+      render json: {}, status: 404 unless params[:auth_token]
+    end
 end
