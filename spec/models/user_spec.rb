1fe56f1b2bedc32d71cbd2ecbd509fffc28160d3
onigra <3280467rec@gmail.com>
2014-04-20 04:19:30 +0900

    auth_tokenのジェネレータのテスト追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,20 @@
 require 'spec_helper'
 
 describe User do
+
+  let(:user) { Fabricate :sample_user }
+
+  describe "generate_token" do
+    before { user.generate_token }
+    subject { user.auth_token }
+
+    it { should_not be_nil }
+  end
+  
+  describe "#destroy_token" do
+    before { user.destroy_token }
+    subject { user.auth_token }
+
+    it { should be_nil }
+  end
 end
