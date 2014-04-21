47e1101741fd53a585945303f78bf2be6bd67cc2
onigra <3280467rec@gmail.com>
2014-04-21 20:06:25 +0900

    books_controllerの異常系のテスト追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+  rescue_from ActionController::UnpermittedParameters, with: :bad_request
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   private
     def auth_token_check
-      render json: {}, status: 404 unless params[:auth_token]
+      render nothing: true, status: 404 unless params[:auth_token]
     end
+
+    def bad_request(exception = nil)
+      logger.info exception.message if exception
+      render nothing: true, status: 400
+    end
+
+    def record_not_found(exception = nil)
+      logger.info exception.message if exception
+      render nothing: true, status: 404
+    end
+
 end
--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -14,34 +14,89 @@ describe "BooksController's Requests" do
   end
 
   describe "GET /books/:id" do
-    it "return book" do
-      get "/books/#{book.to_param}", auth_token: user.auth_token
-      expect(last_response.status).to eq 200
+    context "success" do
+      it "return book" do
+        get "/books/#{book.to_param}", auth_token: user.auth_token
+        expect(last_response.status).to eq 200
+      end
+    end
+
+    context "user not found" do
+      it "return 404" do
+        get "/books/99999999", auth_token: user.auth_token
+        expect(last_response.status).to eq 404
+      end
     end
   end
 
   describe "POST /books" do
     let(:valid_params) { { "name" => "リーダブルコード", "isbn" => "978-4873115658" } }
+    let(:invalid_params) { { "book_name" => "リーダブルコード" } }
 
-    it "create a new book" do
-      post "/books", { book: valid_params, auth_token: user.auth_token }
-      expect(last_response.status).to eq 201
+    context "valid parameters" do
+      it "create a new book" do
+        post "/books", { book: valid_params, auth_token: user.auth_token }
+        expect(last_response.status).to eq 201
+      end
+    end
+
+    context "invalid parameters" do
+      it "return 400" do
+        post "/books", { book: invalid_params, auth_token: user.auth_token }
+        expect(last_response.status).to eq 400
+      end
+    end
+
+    context "save failed" do
+      it "return 500" do
+        Book.any_instance.stub(:save).and_return(false)
+        post "/books", { book: valid_params, auth_token: user.auth_token }
+        expect(last_response.status).to eq 500
+      end
     end
   end
 
   describe "PATCH /books/:id" do
     let(:update_params) { { "name" => "Perfect Ruby" } }
+    let(:invalid_params) { { "book_name" => "リーダブルコード" } }
 
-    it "update a book" do
-      patch "/books/#{book.to_param}", { book: update_params, auth_token: user.auth_token }
-      expect(last_response.status).to eq 200
+    context "valid parameters" do
+      it "update a book" do
+        patch "/books/#{book.to_param}", { book: update_params, auth_token: user.auth_token }
+        expect(last_response.status).to eq 200
+      end
+    end
+
+    context "invalid parameters" do
+      it "return 400" do
+        post "/books", { book: invalid_params, auth_token: user.auth_token }
+        expect(last_response.status).to eq 400
+      end
+    end
+
+    context "save failed" do
+      it "return 500" do
+        Book.any_instance.stub(:update).and_return(false)
+        patch "/books/#{book.to_param}", { book: update_params, auth_token: user.auth_token }
+        expect(last_response.status).to eq 500
+      end
     end
   end
 
   describe "DELETE /books/:id" do
-    it "destroy a book" do
-      delete "/books/#{book.to_param}", auth_token: user.auth_token
-      expect(last_response.status).to eq 204
+    context "success" do
+      it "destroy a book" do
+        delete "/books/#{book.to_param}", auth_token: user.auth_token
+        expect(last_response.status).to eq 204
+      end
+    end
+
+    context "save failed" do
+      it "return 500" do
+        Book.any_instance.stub(:destroy).and_return(false)
+        delete "/books/#{book.to_param}", auth_token: user.auth_token
+        expect(last_response.status).to eq 500
+      end
     end
   end
 
