7ad1c0602eb7145c2940d0337ba2c0382e9e93a1
onigra <3280467rec@gmail.com>
2014-05-07 19:56:45 +0900

    クロスドメイン対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
   rescue_from ActionController::UnpermittedParameters, with: :bad_request
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def current_user
     User.find_by(auth_token: params[:auth_token])
+  end
+
+  # http://blog.rudylee.com/2013/10/29/rails-4-cors/
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = '*'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  def cors_preflight_check
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
+    headers['Access-Control-Max-Age'] = '1728000'
   end
 
   private
